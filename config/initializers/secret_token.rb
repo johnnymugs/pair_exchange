995fc235ae466967209f017055f520a144df16bc
Jake Quain & Jonathan Mukai <pair+jquain+jmukai@pivotallabs.com>
2012-07-04 12:39:31 -0400

    greenfield rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PairExchange::Application.config.secret_token = '71d1f73c03e5332cebe34c21c296b7a9d0530615dea3f1d80584bb88f4c957109104c23b31e555038caf2a2af88aa224d179cfe8ae6ee86e41a787c971c2570e'
