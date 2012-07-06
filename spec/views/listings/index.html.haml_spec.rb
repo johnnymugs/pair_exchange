5e7ff33851e0d09691c35eb5326a4cd702861b6d
quainjn <quainjn@gmail.com>
2012-07-06 16:59:49 -0400

    Hide add new listing link unless logged in [#32386365]

---
+++ spec/views/listings/index.html.haml_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "listings/index" do
+  before do
+    view.stub(:current_user => current_user )
+    @listings = []
+    render
+  end
+
+  let(:current_user) { nil }
+
+  let(:page) { rendered }
+  subject { page }
+
+  describe "new listings link" do
+    context "when the user is logged in" do
+      let(:current_user) { double }
+      it { should contain("New Listing") }
+    end
+
+    context "when the user is not logged in" do
+      let(:current_user) { nil }
+      it { should_not contain("New Listing") }
+    end
+  end
+end
